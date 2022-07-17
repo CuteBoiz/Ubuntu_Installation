c5aa8a37d4259cecef287c7c1029e375bb726a7c
CuteBoiz <tanphat3198@gmail.com>
2022-07-17 07:41:40 +0700

    Upgrade

--- script/opencv.sh
+++ script/opencv.sh
@@ -5,62 +5,50 @@ BRED='\033[1;31m'
 NC='\033[0m'
 OPENCV_LINK="https://github.com/opencv/opencv.git"
 OPENCV_CONTRIB_LINK="https://github.com/opencv/opencv_contrib.git"
+INSTALL_CUDA=0
+
 # Install folder
-INSTALL_DIR=""
-TEMP=0
-while : ; do
-    read -p "$(echo -e $BBLUE"Where to put opencv (defaut ${NC}\"/home/$SUDO_USER/Libraries\"${BBLUE}): $NC")" A
-    if  [[ "$A" == "" ]]; then
-        INSTALL_DIR=/home/$SUDO_USER/Libraries
-    elif [[ "${A::1}" == "." ]]; then
-        INSTALL_DIR="$PWD${A:1}"
-    elif [[ "${A::1}" == "~" ]]; then
-        INSTALL_DIR="/home/$SUDO_USER${A:1}"
-    else
-        INSTALL_DIR=$A
-    fi
-    if [[ $INSTALL_DIR != "" ]]; then
-        while : ; do
-            read -p "$(echo -e $BBLUE"OpenCV will be install in $NC\"$INSTALL_DIR\" $BBLUE(y/n): $NC")" A
-            if [[ "$A" == "y" || "$A" == "Y" ]]; then
-                TEMP=1
-                break
-            elif [[ "$A" == "n" || "$A" == "N" ]]; then
-                TEMP=0
-                break
-            fi
-        done
-    fi
-    if [[ $TEMP -eq 1 ]]; then
-        break
-    fi
-done
+INSTALL_DIR="$HOME/Libraries"
 mkdir -p $INSTALL_DIR && cd $INSTALL_DIR
 echo -e "OpenCV will be install in \"$PWD\""
 sleep 2
-# Use OpenCV_Cuda?
-INSTALL_CUDA=0
+
+# Use OpenCV with Cuda support
 while : ; do
     read -p "$(echo -e $BBLUE"Do you want install OpenCV with CUDA support? (y\\\n): $NC")" A
-    ! [[ "$A" == "y" || "$A" == "Y" || "$A" == "N" || "$A" == "n" ]] || break
-done
-if [[ "$A" == "y" || "$A" == "Y" ]]; then
-    NVIDIA_CHECK="$(lsmod | grep ^nvidia | awk {'print $1'})"
-    if [[ "$NVIDIA_CHECK" ==  *"nvidia"* ]]; then
-        echo -e "Nvidia driver installed!"    
-    else
-        echo -e "${BRED}Error: System has not installed nvidia-driver yet!${NC}"
-        exit 1
+    A=${A^^}
+    if [[ "$A" == "Y" || "$A" == "YES" ]]; then
+        # Check Nvidia driver installed
+        NVIDIA_CHECK="$(lsmod | grep ^nvidia | awk {'print $1'})"
+        if [[ "$NVIDIA_CHECK" ==  *"nvidia"* ]]; then
+            echo -e "Nvidia driver installed!"    
+        else
+            echo -e "${BRED}Error: System has not installed nvidia-driver yet!${NC}"
+            exit 1
+        fi
+        # Check Cuda
+        CUDA_CHECK=$(find /usr/local/cuda-*   -maxdepth 0)
+        CUDA_VER=$(nvcc -V | sed -n 4p | cut -d" " -f5)
+        CUDA_VER=${CUDA_VER:0:4}
+        if [[ "$CUDA_CHECK" == "" ]]; then            
+            echo -e "${BRED}Error: Cuda not installed!${NC}"
+            exit 1
+        else
+            if [[ "$CUDA_VER" == "" ]]; then
+                echo -e "${BRED}Found Cuda installed. But did not add it to \$PATH. Add cuda to $PATH then use \"nvcc -V\" to check!"
+                exit 1
+            else
+                echo -e "Found CUDA-$CUDA_VER"
+                INSTALL_CUDA=1
+            fi
+        fi
+        break
+    elif [[ "$A" == "N" || "$A" == "NO" ]]; then
+        echo -e "Install OpenCV without CUDA support" 
+        break
     fi
-    CUDA_CHECK=$(find /usr/local/cuda-*   -maxdepth 0)
-    if ! [[ "$CUDA_CHECK" == "" ]]; then
-        echo -e "CUDA installed!"
-        INSTALL_CUDA=1
-    else 
-        echo -e "${BRED}Error: Could not found cuda installed!${NC}"
-        exit 1
-    fi 
-fi
+done
+
 
 # Select version
 TAGS=$(git ls-remote --tags $OPENCV_LINK | sed 's/.*\///; s/\^{}//' | sort -u)
@@ -73,7 +61,9 @@ if [[ ${#VERSIONS} == 1 ]]; then
 fi
 INSTALL_VER=""
 while : ; do
-    read -p "$(echo -e $BBLUE"Choose OpenCV Version \n(Press \"l\" to list all available tags)\n(Press Enter to install master branch ): $NC")" A
+    read -p "$(echo -e $BBLUE"Choose OpenCV Version
+${NC}(Press ${BBLUE}l${NC} to list all available tags)
+(Press ${BBLUE}Return/Enter${NC} to install master branch)${BBLUE}: $NC")" A
     if [[ $A == "l" || $A == "L" ]]; then
         for VER in ${VERSIONS_ARR[@]}; do
             echo -e "$VER"
@@ -95,26 +85,25 @@ while : ; do
         fi
     fi
 done
-echo -e "Installing OpenCV-$INSTALL_VER ..."
-sleep 2
-    
+echo -e "Installing OpenCV_$INSTALL_VER ..."
 INSTALL_DIR="$PWD/OpenCV_$INSTALL_VER" 
 mkdir -p $INSTALL_DIR && cd $INSTALL_DIR
+sleep 2
 
 # Clone opencv and opencv_contrib repo
 if ! [ -d opencv ]; then
-    git clone https://github.com/opencv/opencv.git
+    git clone $OPENCV_LINK
 fi
 if ! [ -d opencv_contrib ]; then
-    git clone https://github.com/opencv/opencv_contrib.git
+    git clone $OPENCV_CONTRIB_LINK
 fi
 
 if ! [ -d opencv ]; then
-    echo -d "${BRED}Error: Could not clone opencv from \"https://github.com/opencv/opencv.git\"${NC}"
+    echo -d "${BRED}Error: Could not clone opencv from \"$OPENCV_LINK\"${NC}"
     exit 1
 fi
 if ! [ -d opencv_contrib ]; then
-    echo -d "${BRED}Error: Could not clone opencv_contrib from \"https://github.com/opencv/opencv_contrib.git\"${NC}"
+    echo -d "${BRED}Error: Could not clone opencv_contrib from \"$OPENCV_CONTRIB_LINK\"${NC}"
     exit 1
 fi
 if [[ $INSTALL_VER == "master" ]]; then
@@ -138,7 +127,7 @@ sudo apt-get install -y build-essential cmake git pkg-config libgtk-3-dev \
 # Install
 mkdir -p build && cd build
 if [ -f "CMakeCache.txt" ]; then
-    rm CMakeCache.txt
+    sudo rm CMakeCache.txt
 fi
 if [[ "INSTALL_CUDA" -eq 1 ]]; then
     cmake -D CMAKE_BUILD_TYPE=RELEASE \
@@ -160,7 +149,7 @@ else
         -D PYTHON3_EXECUTABLE=$(which python3) \
         -D PYTHON3_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
         -D PYTHON3_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-        -D OPENCV_EXTRA_MODULES_PATH=$INSTALL_DIR/opencv_contrib/modules \ ..
+        -D OPENCV_EXTRA_MODULES_PATH=$INSTALL_DIR/opencv_contrib/modules ..
 fi
 sudo make -j$(($(nproc) - 1)) 
 sudo make install
